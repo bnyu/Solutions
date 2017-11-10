971246f8d9544810a549b1e4bcd6039817fc3655
bnyu <xuyue06@outlook.com>
2017-11-11 02:56:51 +0800

    p32

--- scr/Solution032.kt
+++ scr/Solution032.kt
@@ -17,18 +17,44 @@ class Solution032 {
                     if (leftLen > 0) {
                         leftLen--
                         rightLen++
+                        //每次满足都比较一次 若仅最大满足比较 右边括号可能不足
+                        if (leftLen == 0) {
+                            if (rightLen > longest)
+                                longest = rightLen
+                        }
                     } else {
-                        val length = 2 * rightLen
-                        if (length > longest)
-                            longest = length
+                        //从上一组最长情况结束开始 这一组也达到最长
+                        if (rightLen > longest)
+                            longest = rightLen
                         rightLen = 0
                     }
                 }
             }
         }
-        //处理最后一组
-        val length = 2 * rightLen
-        return if (length > longest) length else longest
+        //反向判断
+        leftLen = 0
+        rightLen = 0
+        for (i in (s.length - 1) downTo 0) {
+            val c = s[i]
+            when (c) {
+                rightBracket -> rightLen++
+                leftBracket -> {
+                    if (rightLen > 0) {
+                        rightLen--
+                        leftLen++
+                        if (leftLen == 0) {
+                            if (rightLen > longest)
+                                longest = rightLen
+                        }
+                    } else {
+                        if (leftLen > longest)
+                            longest = leftLen
+                        leftLen = 0
+                    }
+                }
+            }
+        }
+        return 2 * longest
     }
 }
 
