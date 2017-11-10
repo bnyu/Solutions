feae54acab4efe674f2b40395a25030b57c785ae
bnyu <xuyue1106@gmail.com>
2017-11-10 16:24:15 +0800

    42 Trapping Rain Water

--- scr/Catalog.kt
+++ scr/Catalog.kt
@@ -32,6 +32,7 @@ enum class Catalog(internal val number: Int, internal val status: Status) {
     CombinationSum(39, Status.Accepted),
     CombinationSum2(40, Status.Accepted),
     FirstMissingPositive(41, Status.Accepted),
+    TrappingRainWater(41, Status.Accepted),
     ;
 
 }
