f7791e41a1709ea9b5b47f1031fac605a5953112
bnyu <xuyue06@outlook.com>
2017-11-26 17:22:13 +0800

    first kata on codewar

--- scala/codewar/NPointCrossover.scala
+++ scala/codewar/NPointCrossover.scala
@@ -8,16 +8,16 @@ object NPointCrossover {
     //从第0个开始交换
     val indexes = List(0) ++ (ns ++ List(xs.size)).sorted.distinct
     cross(indexes, Nil, Nil, xs, ys)
-  }
 
-  @tailrec
-  def cross[T](indexes: List[Int], crossedX: List[T], crossedY: List[T], uncrossedX: List[T], uncrossedY: List[T]): (List[T], List[T]) = {
-    val index = indexes.tail.head - indexes.head
-    val (xl, ys) = uncrossedX.splitAt(index)
-    val (yl, xs) = uncrossedY.splitAt(index)
-    if (indexes.size > 2)
-      cross(indexes.tail, crossedX ++ xl, crossedY ++ yl, xs, ys)
-    else (crossedX ++ xl, crossedY ++ yl)
+    @tailrec
+    def cross(indexes: List[Int], crossedX: List[T], crossedY: List[T], uncrossedX: List[T], uncrossedY: List[T]): (List[T], List[T]) = {
+      val index = indexes.tail.head - indexes.head
+      val (xl, ys) = uncrossedX.splitAt(index)
+      val (yl, xs) = uncrossedY.splitAt(index)
+      if (indexes.size > 2)
+        cross(indexes.tail, crossedX ++ xl, crossedY ++ yl, xs, ys)
+      else (crossedX ++ xl, crossedY ++ yl)
+    }
   }
 }
 
