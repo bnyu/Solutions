1a872499cd1edf34f24fb536dcdbd058b827ff3c
bnyu <xuyue06@outlook.com>
2017-11-26 18:24:59 +0800

    duplicateEncoder

--- scala/codewar/DuplicateEncoder.scala
+++ scala/codewar/DuplicateEncoder.scala
@@ -1,3 +1,24 @@
-object DuplicateEncoder {
+import scala.collection.mutable
 
+/**
+  * The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string,
+  * or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+  */
+object DuplicateEncoder {
+  def duplicateEncode(word: String) = {
+    val str = word.toLowerCase
+    val characters = mutable.HashMap[Char, Int]()
+    val sList = mutable.MutableList.fill(str.length)('(')
+    for (i <- 0 until str.length) {
+      val c = str(i)
+      val index = characters.get(c)
+      if (index.isDefined) {
+        sList(index.get) = ')'
+        sList(i) = ')'
+      } else {
+        characters(c) = i
+      }
+    }
+    sList.foldLeft("")((s, c) => s + c)
+  }
 }
