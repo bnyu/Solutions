73ea2e9c8b215e36a64cb187fb6ce6b698e44e1f
bnyu <xuyue06@outlook.com>
2017-11-26 16:18:27 +0800

    和LeetCode比起来 提交好慢呀

--- scala/codewar/NPointCrossover.scala
+++ scala/codewar/NPointCrossover.scala
@@ -5,22 +5,17 @@ import scala.annotation.tailrec
   */
 object NPointCrossover {
   def crossover[T](ns: List[Int], xs: List[T], ys: List[T]): (List[T], List[T]) = {
-    val indexes = List(0) ++ ns.sorted ++ List(xs.size)
+    val indexes = (List(0) ++ ns ++ List(xs.size)).distinct.sorted
     cross(indexes, Nil, Nil, xs, ys)
   }
 
   @tailrec
   def cross[T](indexes: List[Int], crossedX: List[T], crossedY: List[T], uncrossedX: List[T], uncrossedY: List[T]): (List[T], List[T]) = {
     val index = indexes.tail.head - indexes.head
-    val (xl, yl, xs, ys) = if (index > 0) {
-      val xs = uncrossedY.drop(index)
-      val ys = uncrossedX.drop(index)
-      val xl = crossedX ++ uncrossedX.take(index)
-      val yl = crossedY ++ uncrossedY.take(index)
-      (xl, yl, xs, ys)
-    } else {
-      (crossedX, crossedY, uncrossedX, uncrossedY)
-    }
+    val xs = uncrossedY.drop(index)
+    val ys = uncrossedX.drop(index)
+    val xl = crossedX ++ uncrossedX.take(index)
+    val yl = crossedY ++ uncrossedY.take(index)
     if (indexes.size > 2)
       cross(indexes.tail, xl, yl, xs, ys)
     else (xl, yl)
