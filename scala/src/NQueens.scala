5c99d1a1e6162a733df9b33f62628d8f1f9a1513
bnyu <xuyue06@outlook.com>
2017-11-29 01:19:09 +0800

    N-Queens

---
+++ scala/src/NQueens.scala
@@ -0,0 +1,34 @@
+import scala.collection.mutable
+
+/**
+  * 51. N-Queens
+  * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+  */
+object NQueens {
+
+  private val ans = new mutable.ListBuffer[List[String]]
+
+  def solveNQueens(n: Int): List[List[String]] = {
+    if (n > 0)
+      addQueen(n, 0, Set.empty[Int], Set.empty[Int], Set.empty[Int], Nil)
+    ans.toList
+  }
+
+  private def addQueen(n: Int, row: Int, columnSet: Set[Int], slashSet: Set[Int], backSlashSet: Set[Int], colIndex: List[Int]): Unit = {
+    if (row >= n) {
+      val s: List[String] = colIndex.map { i =>
+        val str = List.fill(i)('.') ++ List('Q') ++ List.fill(n - 1 - i)('.')
+        ("" /: str) (_ + _)
+      }
+      ans.append(s)
+    } else {
+      for (column <- 0 until n) {
+        val slash = row - column
+        val backSlash = row + column
+        if (!(columnSet.contains(column) || slashSet.contains(slash) || backSlashSet.contains(backSlash))) {
+          addQueen(n, row + 1, columnSet + column, slashSet + slash, backSlashSet + backSlash, colIndex ++ List(column))
+        }
+      }
+    }
+  }
+}
