203aa4ee2438f529fbcd075249ac35823511fbeb
xuyue <xuyue1106@gmail.com>
2017-11-30 11:08:23 +0800

    NQueen2 ac

---
+++ scala/src/NQueens2.scala
@@ -0,0 +1,29 @@
+/**
+  * 52. N-Queens II
+  * Follow up for N-Queens problem.
+  * Now, instead outputting board configurations, return the total number of distinct solutions.
+  */
+object NQueens2 {
+  // same as NQueen
+  def totalNQueens(n: Int): Int = {
+    addQueen(n, 0, Set.empty, Set.empty, Set.empty, 0)
+  }
+
+
+  private def addQueen(n: Int, row: Int, columnSet: Set[Int], slashSet: Set[Int], backSlashSet: Set[Int], num: Int): Int = {
+    if (row >= n) {
+      num + 1
+    } else {
+      var validNum = 0
+      for (column <- 0 until n) {
+        val slash = row - column
+        val backSlash = row + column
+        if (!(columnSet.contains(column) || slashSet.contains(slash) || backSlashSet.contains(backSlash))) {
+          validNum += addQueen(n, row + 1, columnSet + column, slashSet + slash, backSlashSet + backSlash, num)
+        }
+      }
+      num + validNum
+    }
+  }
+}
+
