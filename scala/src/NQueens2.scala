d91005b76e0651da3dac3e5149b36ba40c5c5042
xuyue <xuyue1106@gmail.com>
2017-11-30 11:24:42 +0800

    s52

--- scala/src/NQueens2.scala
+++ scala/src/NQueens2.scala
@@ -6,23 +6,23 @@
 object NQueens2 {
   // same as NQueen
   def totalNQueens(n: Int): Int = {
-    addQueen(n, 0, Set.empty, Set.empty, Set.empty, 0)
+    addQueen(n, 0, Set.empty, Set.empty, Set.empty)
   }
 
 
-  private def addQueen(n: Int, row: Int, columnSet: Set[Int], slashSet: Set[Int], backSlashSet: Set[Int], num: Int): Int = {
+  private def addQueen(n: Int, row: Int, columnSet: Set[Int], slashSet: Set[Int], backSlashSet: Set[Int]): Int = {
     if (row >= n) {
-      num + 1
+      1
     } else {
       var validNum = 0
       for (column <- 0 until n) {
         val slash = row - column
         val backSlash = row + column
         if (!(columnSet.contains(column) || slashSet.contains(slash) || backSlashSet.contains(backSlash))) {
-          validNum += addQueen(n, row + 1, columnSet + column, slashSet + slash, backSlashSet + backSlash, num)
+          validNum += addQueen(n, row + 1, columnSet + column, slashSet + slash, backSlashSet + backSlash)
         }
       }
-      num + validNum
+      validNum
     }
   }
 }
