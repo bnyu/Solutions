493c293a5a7da8984b22aa9c4834ad413ba3240d
xuyue <xuyue1106@gmail.com>
2017-12-11 01:43:02 +0800

    4. Median of Two Sorted Arrays

---
+++ kotlin/src/MedianOfTwoSortedArrays.kt
@@ -0,0 +1,40 @@
+/**
+ * 4. Median of Two Sorted Arrays
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ */
+class MedianOfTwoSortedArrays {
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val len1 = nums1.size
+        val len2 = nums2.size
+        val mid2 = (len1 + len2) / 2
+        val mid1 = if ((len1 + len2) % 2 == 0) mid2 - 1 else mid2
+        val ans = if (len2 != 0) {
+            var x = 0
+            var y = 0
+            for (i in 0 until mid1) {
+                if (x < len1 && nums1[x] <= nums2[y]) x++
+                else y++
+            }
+            //到了第中位数
+            val a = if (x < len1 && nums1[x] <= nums2[y]) nums1[x++]
+            else nums2[y++]
+            //判断是否越界
+            val b = when {
+                mid2 == mid1 -> a
+                x < len1 && y < len2 && nums1[x] <= nums2[y] -> nums1[x]
+                x < len1 && y < len2 -> nums2[y]
+                x == len1 -> nums2[y]
+                else -> nums1[x]
+            }
+            (a + b).toDouble() / 2
+        } else {
+            if (len1 != 0) {
+                val a = nums1[mid1]
+                val b = nums1[mid2]
+                (a + b).toDouble() / 2
+            } else 0.0
+        }
+        return ans
+    }
+}
