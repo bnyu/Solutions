675c0db6c370b5ee9ac89a8d045af9df8f8dcc00
xuyue <xuyue1106@gmail.com>
2017-12-12 00:57:42 +0800

    61. Rotate List AC

--- scala/src/RotateList.scala
+++ scala/src/RotateList.scala
@@ -6,15 +6,15 @@ object RotateList {
   def rotateRight(head: ListNode, k: Int): ListNode = {
     //倒数次序 k前一个node 尾node
     def findNode(head: ListNode, k: Int): (Int, ListNode, ListNode) = {
-      val x = if (head.next == null) (0, head, head)
-      else findNode(head.next, k)
+      val x = if (head.next == null) (0, head, head) else findNode(head.next, k)
       //退出栈每次+1 仅在k前一个位置改变node并保持
-      if (x._1 == k) (x._1 + 1, head, x._3)
-      else (x._1 + 1, x._2, x._3)
+      if (x._1 == k) (x._1 + 1, head, x._3) else (x._1 + 1, x._2, x._3)
     }
 
+    //居然要求考虑k大于链表长度的情况...
     if (head == null) null else {
-      val (_, node, tail) = findNode(head, k)
+      val (len, preNode, preTail) = findNode(head, k)
+      val (_, node, tail) = if (k > len) findNode(head, k % len) else (len, preNode, preTail)
       tail.next = head
       val newHead = node.next
       node.next = null
@@ -22,3 +22,4 @@ object RotateList {
     }
   }
 }
+
