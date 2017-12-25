d8389ba45b6214576668634c20529c55d1bfe8ce
xuyue <xuyue1106@gmail.com>
2017-12-25 14:39:52 +0800

    加缓存

--- scala/src/UniquePaths.scala
+++ scala/src/UniquePaths.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /** 62. Unique Paths
   * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
   * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
@@ -5,13 +7,16 @@
   */
 object UniquePaths {
   def uniquePaths(m: Int, n: Int): Int = {
+    val paths: mutable.HashMap[(Int, Int), Int] = mutable.HashMap()
 
     def upMove(m: Int, n: Int): Int = {
-      if (m <= 0) 0 else if (m == 1) 1 else upMove(m - 1, n) + leftMove(m, n - 1)
+      if (m <= 0) 0 else if (m == 1) 1 else
+        paths.getOrElseUpdate((m - 1, n), upMove(m - 1, n)) + paths.getOrElseUpdate((m, n - 1), leftMove(m, n - 1))
     }
 
     def leftMove(m: Int, n: Int): Int = {
-      if (n <= 0) 0 else if (n == 1) 1 else upMove(m - 1, n) + leftMove(m, n - 1)
+      if (n <= 0) 0 else if (n == 1) 1 else
+        paths.getOrElseUpdate((m - 1, n), upMove(m - 1, n)) + paths.getOrElseUpdate((m, n - 1), leftMove(m, n - 1))
     }
 
     if (m <= 0 || n <= 0) 0 else if (m == 1 || n == 1) 1 else leftMove(m, n - 1) + upMove(m - 1, n)
