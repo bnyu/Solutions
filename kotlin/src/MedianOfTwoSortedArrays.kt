5e8e43bccfc04410df841a8c8485ea22421f61a5
xuyue <xuyue1106@gmail.com>
2017-12-25 22:19:46 +0800

    4
    Signed-off-by: xuyue <xuyue1106@gmail.com>

--- kotlin/src/MedianOfTwoSortedArrays.kt
+++ kotlin/src/MedianOfTwoSortedArrays.kt
@@ -9,38 +9,42 @@ class MedianOfTwoSortedArrays {
         val len2 = nums2.size
         val mid2 = (len1 + len2) / 2
         val mid1 = if ((len1 + len2) % 2 == 0) mid2 - 1 else mid2
-        val ans = if (len2 != 0) {
-            var x = 0
-            var y = 0
-            for (i in 0 until mid1) {
-                when {
-                    x >= len1 -> y++
-                    y >= len2 -> x++
-                    nums1[x] <= nums2[y] -> x++
-                    else -> y++
+        return when {
+            len2 != 0 -> {
+                var x = 0
+                var y = 0
+                for (i in 0 until mid1) {
+                    when {
+                        x >= len1 -> y++
+                        y >= len2 -> x++
+                        nums1[x] <= nums2[y] -> x++
+                        else -> y++
+                    }
                 }
+                //到了第中位数
+                val a = when {
+                    x < len1 && y < len2 && nums1[x] <= nums2[y] -> nums1[x++]
+                    x < len1 && y < len2 -> nums2[y++]
+                    x >= len1 -> nums2[y++]
+                    else -> nums1[x++]
+                }
+                val b = when {
+                    mid2 == mid1 -> a
+                    x < len1 && y < len2 && nums1[x] <= nums2[y] -> nums1[x]
+                    x < len1 && y < len2 -> nums2[y]
+                    x >= len1 -> nums2[y]
+                    else -> nums1[x]
+                }
+                (a + b).toDouble() / 2
             }
-            //到了第中位数 判断是否越界
-            val a = when {
-                x < len1 && y < len2 && nums1[x] <= nums2[y] -> nums1[x++]
-                x < len1 && y < len2 -> nums2[y++]
-                x >= len1 -> nums2[y++]
-                else -> nums1[x++]
+            len1 != 0 -> {
+                val a = nums1[mid1]
+                val b = nums1[mid2]
+                (a + b).toDouble() / 2
             }
-            val b = when {
-                mid2 == mid1 -> a
-                x < len1 && y < len2 && nums1[x] <= nums2[y] -> nums1[x]
-                x < len1 && y < len2 -> nums2[y]
-                x >= len1 -> nums2[y]
-                else -> nums1[x]
-            }
-            (a + b).toDouble() / 2
-        } else if (len1 != 0) {
-            val a = nums1[mid1]
-            val b = nums1[mid2]
-            (a + b).toDouble() / 2
-        } else 0.0
-        return ans
+            else -> 0.0
+        }
     }
 }
+
 
