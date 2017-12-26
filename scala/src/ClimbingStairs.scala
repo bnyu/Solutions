b8be315960b6978447cc127636f8bc07456421dd
xuyue <xuyue1106@gmail.com>
2017-12-26 16:58:57 +0800

    Climbing Stairs(Fibonacci sequence)

--- scala/src/ClimbingStairs.scala
+++ scala/src/ClimbingStairs.scala
@@ -1,5 +1,3 @@
-import scala.collection.mutable
-
 /** 70. Climbing Stairs
   * You are climbing a stair case. It takes n steps to reach to the top.
   * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -7,17 +5,9 @@ import scala.collection.mutable
   */
 object ClimbingStairs {
   def climbStairs(n: Int): Int = {
-    val cache = mutable.HashMap[Int, Int]()
+    //n:剩余阶梯数 x:到这步总数 x0:上一步时总数 (Fibonacci sequence)
+    def climb(n: Int, x: Int, x0: Int): Int = if (n < 0) x else climb(n - 1, x + x0, x)
 
-    //n:剩余阶梯数 x:已走步数
-    def climb(n: Int, x: Int): Int = if (n < 0) x else {
-      // 上一步加上上上步
-      val steps = x + cache.getOrElse(n + 2, 0)
-      cache.update(n, steps)
-      //接着走下一步
-      climb(n - 1, steps)
-    }
-
-    if (n > 0) climb(n, 1) else 0
+    if (n > 2) climb(n - 3, 2, 1) else if (n == 2) 2 else if (n == 1) 1 else 0
   }
 }
