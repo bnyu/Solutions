3d6b1c1224b66c2138fb4d7b8763b8af869fc305
xuyue <xuyue1106@gmail.com>
2017-12-26 23:33:39 +0800

    AC
    Signed-off-by: xuyue <xuyue1106@gmail.com>

--- scala/src/SimplifyPath.scala
+++ scala/src/SimplifyPath.scala
@@ -9,19 +9,20 @@ object SimplifyPath {
     val slash = '/'
     val dot = '.'
     val paths = mutable.HashMap[Int, String]()
-
+    var start = 1
     def enterPath(path: String, index: Int): Int = {
       if (path != null && path.nonEmpty) {
         val head = path.head
         val tail = path.tail
         if (head == slash)
           enterPath(tail, index)
-        //ok ... 居然是合法路径
+        //ok ... 居然是合法路径 多个///忽略为/
         else if (head == dot && tail != null && tail.nonEmpty && tail.head == dot && (tail.tail == null || tail.tail.isEmpty || tail.tail.head == slash))
           enterPath(tail.tail, index - 1)
         else if (head == dot && (tail == null || tail.isEmpty || tail.head == slash))
           enterPath(tail, index)
         else {
+          if (index < start) start = index
           val i = path.indexWhere(_ == slash)
           val (head, tail) = if (i > 0) (path.take(i), path.drop(i + 1)) else (path, null)
           paths.update(index, head)
@@ -30,16 +31,16 @@ object SimplifyPath {
       } else index
     }
 
-    // 不用考虑输入路径曾到达上级目录 而且输入必须以“/”开始 即index>0
+    //路径曾到达上级目录 此时以最上层作为初始根目录输出
     val index = enterPath(path, 1)
-    var i = 0
+    var i = start - 1
 
     def pathStr = {
       i += 1
       paths(i)
     }
 
-    val list = Array.fill(index - 1)(pathStr)
+    val list = Array.fill(index - start)(pathStr)
     list.mkString("/", "/", "")
   }
 }
