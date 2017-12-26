0fa1718b1d388c8d0eed3f6f9209f43692f5fe4a
xuyue <xuyue1106@gmail.com>
2017-12-26 15:58:14 +0800

    Climbing Stairs Ac

---
+++ scala/src/ClimbingStairs.scala
@@ -0,0 +1,18 @@
+import scala.collection.mutable
+
+/** 70. Climbing Stairs
+  * You are climbing a stair case. It takes n steps to reach to the top.
+  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+  * Note: Given n will be a positive integer.
+  */
+object ClimbingStairs {
+  def climbStairs(n: Int): Int = {
+    val cache = mutable.HashMap[Int, Int]()
+
+    def climb(n: Int): Int = {
+      if (n < 0) 0 else if (n == 0) 1 else cache.getOrElseUpdate(n, climb(n - 1) + climb(n - 2))
+    }
+
+    climb(n)
+  }
+}
