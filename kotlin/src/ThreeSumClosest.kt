52408711ef509ec0c78fad467588a65eb9e703ec
xuyue <xuyue1106@gmail.com>
2018-01-05 02:29:47 +0800

    3 Sum Closest
    Signed-off-by: xuyue <xuyue1106@gmail.com>

--- kotlin/src/ThreeSumClosest.kt
+++ kotlin/src/ThreeSumClosest.kt
@@ -7,47 +7,77 @@ class ThreeSumClosest {
     fun threeSumClosest(nums: IntArray, target: Int): Int {
         val size = nums.size
         if (size < 3) return 0
-        quickSort(nums, 0, size - 1)
-        val smallest = nums[0] + nums[1] + nums[2]
-        val biggest = nums[size - 1] + nums[size - 2] + nums[size - 3]
+        val numList = nums.toMutableList()
+        quickSort(numList, 0, size - 1)
+        val smallest = numList[0] + numList[1] + numList[2]
+        val biggest = numList[size - 1] + numList[size - 2] + numList[size - 3]
 
-        if (smallest >= target)
+        var sumPair = if (smallest >= target)
             return smallest
         else if (biggest <= target)
             return biggest
+        else {
+            val sDiff = Math.abs(smallest - target)
+            val bDiff = Math.abs(biggest - target)
+            if (sDiff <= bDiff) Pair(sDiff, smallest) else Pair(bDiff, biggest)
+        }
 
-        val partSumMap = mutableMapOf<Int, Int>()
-        val partSumList = mutableListOf<Int>()
-        var preFirst: Int? = null
-        for (i in 0 until nums.size - 2) {
-            val first = nums[i]
-            if (first == preFirst)
-                continue
-            preFirst = first
-            var preSecond: Int? = null
-            for (j in i + 1 until nums.size - 1) {
-                val second = nums[j]
-                if (second == preSecond)
-                    continue
-                preSecond = second
-                val partSum = first + second
-                // 相同partSum 覆盖之前的 即取j更小的情况
-                partSumMap.put(partSum, j)
-                partSumList.add(partSum)
+
+        val numNum = mutableListOf<Pair<Int, Int>>()
+        var preNum = numList[0]
+        var n = 1
+        for (i in 1 until size) {
+            val num = numList[i]
+            if (preNum != num) {
+                numNum.add(Pair(preNum, n))
+                preNum = num
+                n = 0
+            }
+            ++n
+        }
+        numNum.add(Pair(numList[size - 1], n))
+
+        val uniqueSize = numNum.size
+        val partSumList = mutableListOf<Triple<Int, Int, Int>>()
+        for (i in 0 until uniqueSize) {
+            val a = numNum[i].first
+            val aNum = numNum[i].second
+            val index = if (aNum == 1) i + 1 else i
+            for (j in index until uniqueSize) {
+                val b = numNum[j].first
+                val bNum = numNum[j].second
+                val partSum = a + b
+                partSumList.add(Triple(partSum, bNum, j))
             }
         }
 
-        val partSums = partSumList.toIntArray()
-        quickSort(partSums, 0, partSums.size - 1)
-        var i = size - 1
-        var k = partSums.size - 1
-
-        //todo temp
-        return 0
-
+        for (triple in partSumList) {
+            val partNum = triple.first
+            val needNum = target - partNum
+            var first = if (triple.second <= 2) triple.third + 1 else triple.third
+            var end = uniqueSize - 1
+            if (first <= end && numList[end] >= needNum && numList[first] <= needNum) {
+                while (end - first > 1) {
+                    val mid = (end + first) / 2
+                    val c = numList[mid]
+                    if (needNum == c)
+                        return target
+                    if (needNum > c)
+                        end = mid
+                    else
+                        first = mid
+                }
+                val fDiff = Math.abs(numList[first] - needNum)
+                val eDiff = Math.abs(numList[end] - needNum)
+                when {fDiff <= eDiff && fDiff < sumPair.first -> sumPair = Pair(fDiff, partNum + numList[first])
+                    eDiff < sumPair.first -> sumPair = Pair(eDiff, partNum + numList[end])
+                }
+            }
+        }
+        return sumPair.second
     }
 
-    private fun quickSort(nums: IntArray, first: Int, end: Int) {
+    private fun quickSort(nums: MutableList<Int>, first: Int, end: Int) {
         var i = first
         var k = end
         val mid = nums[k]
