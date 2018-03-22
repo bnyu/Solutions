2d20a7308883640b681caa6d31e55c98c2ce1116
bnyu <xuyue1106@gmail.com>
2018-03-23 00:04:48 +0800

    use only O(k) extra space

--- scala/src/PascalsTriangleII.scala
+++ scala/src/PascalsTriangleII.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /** 119. Pascal's Triangle II
   * Given an index k, return the kth row of the Pascal's triangle.
   * For example, given k = 3, Return [1,3,3,1].
@@ -5,15 +7,17 @@
   */
 object PascalsTriangleII {
   def getRow(rowIndex: Int): List[Int] = {
-    var row: List[Int] = Nil
-    for (_ <- 0 to rowIndex) {
-      var pre = 0
-      row = row.map(x => {
-        val elem = pre + x
+    val row = mutable.MutableList.fill(rowIndex + 1)(1)
+    var index = 1
+    for (_ <- 1 until rowIndex) {
+      var pre = 1
+      for (i <- 1 to index) {
+        val x = row(i)
+        row(i) = pre + x
         pre = x
-        elem
-      }) ::: 1 :: Nil
+      }
+      index += 1
     }
-    row
+    row.toList
   }
 }
