295e2fc7fe4a494adf78caef2bf979e8758c662c
bnyu <xuyue1106@gmail.com>
2018-03-22 23:42:50 +0800

    this problem, LeetCode only supports Java submission (JVM language)

---
+++ java/src/TreeLinkNode.java
@@ -0,0 +1,7 @@
+// Definition for binary tree with next pointer.
+// About this, LeetCode only has Java API now (JVM language)
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) { val = x; }
+}
--- kotlin/test/Test.kt
+++
@@ -1,8 +0,0 @@
-object Test {
-    @JvmStatic
-    fun main(arg: Array<String>) {
-        val threeSum = ThreeSumClosest()
-        val ans = threeSum.threeSumClosest(intArrayOf(-1,0,1,1,10), 3)
-        print(ans)
-    }
-}
