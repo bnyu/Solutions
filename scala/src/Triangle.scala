39d3153531e63994a44c48a896ff16d75efb472e
bnyu <xuyue1106@gmail.com>
2018-03-23 00:08:57 +0800

    TLE still

--- scala/src/Triangle.scala
+++ scala/src/Triangle.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /** 120. Triangle
   * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
   * For example, given the following triangle: [[2],[3,4],[6,5,7],[4,1,8,3]]
@@ -7,26 +9,42 @@
 object Triangle {
   def minimumTotal(triangle: List[List[Int]]): Int = {
     val depth = triangle.length
-    var initialized = false
-    var min = 0
+    var min: Option[Int] = None
+    val cache = new mutable.HashMap[(Int, Int), Int]()
+
+    def continue(level: Int, index: Int, sum: Int): Boolean = {
+      cache.get((level, index)) match {
+        case None =>
+          cache.put((level, index), sum)
+          true
+        case Some(s) if sum < s =>
+          cache.put((level, index), sum)
+          true
+        case _ => false
+      }
+    }
 
     def pathSum(level: Int, index: Int, sum: Int): Unit = {
       if (level == depth) {
-        if (initialized) {
-          min = if (sum < min) sum else min
-        } else {
-          initialized = true
-          min = sum
+        min match {
+          case None => min = Some(sum)
+          case Some(s) if s > sum => min = Some(sum)
+          case _ => Unit
         }
       } else {
-        pathSum(level + 1, index, sum + triangle(level)(index))
-        if (index + 1 < triangle(level).length)
-          pathSum(level + 1, index + 1, sum + triangle(level)(index + 1))
+        val sum2 = sum + triangle(level)(index)
+        if (continue(level, index, sum2))
+          pathSum(level + 1, index, sum2)
+        if (triangle(level).lengthCompare(index + 1) > 0) {
+          val sum3 = sum + triangle(level)(index + 1)
+          if (continue(level, index + 1, sum3))
+            pathSum(level + 1, index + 1, sum3)
+        }
       }
     }
 
     pathSum(0, 0, 0)
-    min
+    min.getOrElse(0)
   }
 }
 
