d898fa09b8c0cadd94c91bda70a0da35702d14ee
bnyu <xuyue1106@gmail.com>
2018-04-04 00:09:05 +0800

    MutableList is more efficient than List at adding!

--- scala/src/ReorderList.scala
+++ scala/src/ReorderList.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /** 143. Reorder List
   * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
   * reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
@@ -6,11 +8,11 @@
   */
 object ReorderList {
   def reorderList(head: ListNode): Unit = {
-    var nodes: List[ListNode] = Nil
+    val nodes = new mutable.MutableList[ListNode]
     var node = head
     while (node != null) {
       val pre = node
-      nodes = nodes ::: pre :: Nil
+      nodes += pre
       node = pre.next
       pre.next = null
     }
