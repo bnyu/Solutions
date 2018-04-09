00dd01fabdfc98e97135800c74aa691321e752e7
bnyu <xuyue1106@gmail.com>
2018-04-09 23:50:18 +0800

    AC BTree MaxPathSum

--- scala/src/BinaryTreeMaximumPathSum.scala
+++ scala/src/BinaryTreeMaximumPathSum.scala
@@ -1,5 +1,3 @@
-import scala.collection.mutable
-
 /** 124. Binary Tree Maximum Path Sum
   * Given a binary tree, find the maximum path sum.
   * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
@@ -9,64 +7,33 @@ import scala.collection.mutable
   * - 2   3
   */
 object BinaryTreeMaximumPathSum {
-
-  private class ThreeNode(val value: TreeNode) {
-    val link: Array[ThreeNode] = Array.fill[ThreeNode](3)(null)
-
-    def addLeft(left: ThreeNode): Unit = {
-      this.link(1) = left
-      left.link(0) = this
-    }
-
-    def addRight(right: ThreeNode): Unit = {
-      this.link(2) = right
-      right.link(0) = this
-    }
-  }
-
   def maxPathSum(root: TreeNode): Int = {
     if (root != null) {
       var singleMax = root.value
-      var max = 0
-      val edgeNodes = new mutable.HashSet[ThreeNode]
+      var totalMax = 0
 
-      def createThreeNode(root: ThreeNode, treeNode: TreeNode): Unit = {
-        if (treeNode.left != null) {
-          val left = new ThreeNode(treeNode.left)
-          if (left.value.value > 0)
-            edgeNodes.add(left)
-          if (left.value.value > singleMax)
-            singleMax = left.value.value
-          root.addLeft(left)
-          createThreeNode(left, treeNode.left)
-        }
-        if (treeNode.right != null) {
-          val right = new ThreeNode(treeNode.right)
-          if (right.value.value > 0)
-            edgeNodes.add(right)
-          if (right.value.value > singleMax)
-            singleMax = right.value.value
-          root.addRight(right)
-          createThreeNode(right, treeNode.right)
-        }
+      def pathSum(root: TreeNode): Int = {
+        if (root != null) {
+          if (root.value > singleMax) singleMax = root.value //要求至少连接一个节点 但每个节点最大路径都抛弃了负值(避免全部节点为负)
+          val left = pathSum(root.left)
+          val right = pathSum(root.right)
+          if (left > 0 && right > 0) {
+            val total = left + root.value + right
+            if (total > totalMax)
+              totalMax = total
+          } //转折节点 因为是二叉树 所以只能在一个节点转折(向上再到向下) 因此不需要像之前那样转化成图
+          val max = root.value + {
+            if (left > 0 || right > 0) if (left > right) left else right else 0
+          } //每个节点最大路径等于它较大的子节点最大路径+节点值 以此类推
+          if (max > 0) { //若某节点最大为负 则停止往下加 即不返回负值
+            if (max > totalMax) totalMax = max
+            max
+          } else 0
+        } else 0
       }
 
-
-      //是最小支撑树,只要不往回走不会重复,所以不用记录状态
-      def travelThreeNode(root: ThreeNode, sum: Int, from: ThreeNode): Unit = {
-        if (sum > max)
-          max = sum
-        for (node <- root.link if node != null && node != from)
-          travelThreeNode(node, sum + node.value.value, root)
-      }
-
-      val threeRoot = new ThreeNode(root)
-      if (root.value > 0)
-        edgeNodes.add(threeRoot)
-      createThreeNode(threeRoot, root)
-      for (start <- edgeNodes)
-        travelThreeNode(start, start.value.value, null)
-      if (singleMax < 0) singleMax else max
+      pathSum(root)
+      if (singleMax <= 0) singleMax else totalMax
     } else 0
   }
 }
