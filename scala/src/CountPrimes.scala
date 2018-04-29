3e203129fce9f9bb4371f2b38acdb3de6d235864
bnyu <xuyue1106@gmail.com>
2018-04-30 01:48:36 +0800

    slightly optimized

--- scala/src/CountPrimes.scala
+++ scala/src/CountPrimes.scala
@@ -7,11 +7,17 @@ object CountPrimes {
   def countPrimes(n: Int): Int = {
     if (n <= 1) return 0
     val nonPrime = new Array[Boolean](n) //不包括n
-    for (x <- 2 until n if x * x <= n) { //开根 乘法比除法高效 但若n很大x*x可能会溢出 所以必须是until
-      if (!nonPrime(x)) { //已经是合数
-        for (i <- x + x until n by x) { //排除自身
-          nonPrime(i) = true //将其倍数设为合数
+    var loop = true
+    var x = 2
+    while (loop) { //改成while就不会溢出了
+      if (x * x > n)
+        loop = false
+      else {
+        if (!nonPrime(x)) { //已经是合数
+          for (i <- x + x until n by x) //排除自身
+            nonPrime(i) = true //将其倍数设为合数
         }
+        x += 1
       }
     }
     nonPrime.count({ b => !b }) - 2 //减去0和1
