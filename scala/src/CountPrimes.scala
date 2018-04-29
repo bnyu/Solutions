33e3caba754de74ed821a8f2d3f541af41d0703e
bnyu <xuyue1106@gmail.com>
2018-04-29 23:40:14 +0800

    TLE

--- scala/src/CountPrimes.scala
+++ scala/src/CountPrimes.scala
@@ -6,35 +6,41 @@ import scala.collection.mutable
   * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
   */
 object CountPrimes {
-  private val odds = new mutable.MutableList[Int]
-  odds += 2
-  odds += 3
-  private var maxOdd = 3
-  private var maxN = 4
-
   def countPrimes(n: Int): Int = {
     val x = n - 1
-    if (x > maxN) {
-      for (i <- maxN + 1 to x by 2) {
+    //静态字段会有多线程问题
+    val littleOdds = new mutable.MutableList[Int]
+    littleOdds += 2
+    for (i <- 3 to 17 by 2) {
+      var isOdd = true
+      for (odd <- littleOdds if isOdd && odd <= i / 2) {
+        if (i % odd == 0)
+          isOdd = false
+      }
+      if (isOdd)
+        littleOdds += i
+    }
+
+
+    if (x >= 17) {
+      val bigOdds = new mutable.MutableList[Int]
+      for (i <- 19 to x by 2) {
         var isOdd = true
-        for (odd <- odds if isOdd && odd <= i / 3) {
+        for (odd <- littleOdds if isOdd) {
+          if (i % odd == 0)
+            isOdd = false
+        }
+        for (odd <- bigOdds if isOdd && odd <= i / 17) {
           if (i % odd == 0)
             isOdd = false
         }
         if (isOdd) {
-          odds += i
-          maxOdd = i
+          bigOdds += i
         }
       }
-      if ((x | 1) == 1)
-        maxN = x + 1
-      else
-        maxN = x
-      odds.length
-    } else if (x >= maxOdd) {
-      odds.length
-    } else {
-      odds.indexWhere(_ > x)
+      littleOdds.length + bigOdds.length
+    } else  {
+      littleOdds.indexWhere(_ > x)
     }
   }
 }
