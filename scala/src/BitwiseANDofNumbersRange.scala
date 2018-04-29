2ff59ec838fede13424a33b5bd9c2e7559789632
bnyu <xuyue1106@gmail.com>
2018-04-29 15:27:09 +0800

    fixed

--- scala/src/BitwiseANDofNumbersRange.scala
+++ scala/src/BitwiseANDofNumbersRange.scala
@@ -4,24 +4,33 @@
   */
 object BitwiseANDofNumbersRange {
   def rangeBitwiseAnd(m: Int, n: Int): Int = {
-    //only 3 kinds:if m equal n,else if has same highest bit, else 0
+    //only 3 kinds:if m equal n,else if has same highest bits, else 0
     if (m == 0)
       0
     else if (m == n) {
       m
     } else {
-      def highestBit(n: Int): Int = {
+      def highestBits(n: Int): Int = {
         var x = n
         var i = 0
         while (x != 0) {
           x = x >> 1
           i += 1
         }
-        1 << (i - 1)
+        x = 1 << (i - 1)
+        while (i > 1) {
+          i -= 1
+          val y = x | (x >> 1)
+          if ((y & n) == y)
+            x = y
+          else
+            i = 0
+        }
+        x
       }
 
-      val x1 = highestBit(n)
-      val x2 = highestBit(m)
+      val x1 = highestBits(n)
+      val x2 = highestBits(m)
       x1 & x2
     }
   }
