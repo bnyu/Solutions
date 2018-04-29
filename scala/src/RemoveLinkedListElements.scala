1ef4c57a60a0a645b95c3603fefd9a6de5f5c6f3
bnyu <xuyue1106@gmail.com>
2018-04-29 16:45:33 +0800

    203

---
+++ scala/src/RemoveLinkedListElements.scala
@@ -0,0 +1,22 @@
+/** 203. Remove Linked List Elements
+  * Remove all elements from a linked list of integers that have value val.
+  * Example: Input:  1->2->6->3->4->5->6, val = 6  Output: 1->2->3->4->5
+  */
+object RemoveLinkedListElements {
+  def removeElements(head: ListNode, `val`: Int): ListNode = {
+    val temp = new ListNode()
+    temp.next = head
+    var pre = temp
+    var node = temp.next
+    while (node != null) {
+      if (node.x == `val`) {
+        pre.next = null
+      } else {
+        pre.next = node
+        pre = node
+      }
+      node = node.next
+    }
+    temp.next
+  }
+}
