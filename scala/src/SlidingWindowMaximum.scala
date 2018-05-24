8d9743106718997121beae0600fd7055f7e0fdc5
bnyu <xuyue1106@gmail.com>
2018-05-25 00:41:19 +0800

    TLE

--- scala/src/SlidingWindowMaximum.scala
+++ scala/src/SlidingWindowMaximum.scala
@@ -1,5 +1,3 @@
-import scala.collection.mutable
-
 /** 239. Sliding Window Maximum
   * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
   * Example: Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3   Output: [3,3,5,5,6,7]
@@ -29,40 +27,40 @@ object SlidingWindowMaximum {
       val ans = new Array[Int](nums.length - k + 1)
       var maxIndex = 0
       var max = nums(0)
-      val after = new mutable.Queue[(Int, Int)]() //only need two
+      var second: Option[(Int, Int)] = None
+      var last: Option[(Int, Int)] = None
 
       for (i <- nums.indices) {
         if (maxIndex + k <= i) {
-          val second = after.dequeue()
-          max = second._1
-          maxIndex = second._2
+          max = second.get._1
+          maxIndex = second.get._2
+          second = last
+          last = None
         }
 
         val n = nums(i)
-        println(n)
         if (n >= max) {
           max = n
           maxIndex = i
-          after.clear()
-        } else if (after.nonEmpty) {
-          val second = after.head
-          if (n >= second._1) {
-            after.clear()
-            after.enqueue((n, i))
+          second = None
+          last = None
+        } else if (second.isDefined) {
+          if (n >= second.get._1) {
+            second = Some((n, i))
+            last = None
           } else {
-            val last = after.last
-            if (n > last._1) {
-              after.dropRight(1)
-              after.enqueue((n, i))
+            if (last.isEmpty)
+              last = Some((n, i))
+            else if (n >= last.get._1) {
+              last = Some((n, i))
             }
           }
         } else {
-          after.enqueue((n, i))
+          second = Some((n, i))
         }
         val index = i + 1 - k
         if (index >= 0) {
           ans(index) = max
-          println("====" + max)
         }
       }
       ans
