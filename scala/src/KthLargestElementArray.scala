76371c04b49ca9a1e90c91bfeea87ec9cd590b49
bnyu <xuyue1106@gmail.com>
2018-09-23 00:22:10 +0800

    kth largest

--- scala/src/KthLargestElementArray.scala
+++ scala/src/KthLargestElementArray.scala
@@ -2,20 +2,19 @@ import scala.collection.mutable
 
 object KthLargestElementArray {
   def findKthLargest(nums: Array[Int], k: Int): Int = {
-    val set = new mutable.TreeSet[Int]()
+    val map = new mutable.TreeMap[Int, Int]()
     var kth = 0
+    var size = 0
     for (n <- nums) {
-      if (set.size < k) {
-        if (!set.contains(n)) {
-          set.add(n)
-          kth = set.head
-        }
+      if (size < k) {
+        map(n) = map.getOrElse(n, 0) + 1
+        size += 1
+        kth = map.head._1
       } else if (n > kth) {
-        if (!set.contains(n)) {
-          set.remove(kth)
-          set.add(n)
-          kth = set.head
-        }
+        val num = map(kth)
+        if (num > 1) map(kth) = num - 1 else map.remove(kth)
+        map(n) = map.getOrElse(n, 0) + 1
+        kth = map.head._1
       }
     }
     kth
